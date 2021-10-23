8c900bba577cda3ef5ce227888e2d84bbba56f63
suhaib200110 <suhaibsalman200110@gmial.com>
2021-10-23 12:27:56 +0500

    Task#4 of Lab#5 added

--- OOP Lab/Assi#5/LT#4.cpp
+++ OOP Lab/Assi#5/LT#4.cpp
@@ -1,13 +1,55 @@
 #include <iostream>
-#define DECLDIREXP __declspec(dllexport) 
 using namespace std;
 
-class DECLDIREXP word
+class word
 {
-    string name;
+    string random_string;
     public:
     word()
     {
+        random_string = "Muhammad Suhaib Salman";
+    }
+    word (string name)
+    {
+        random_string = name;
+    }
+    void display()
+    {
+        cout<<"\n\nThe string is: "<<random_string<<endl;
+    }
+    word operator + (word x)
+    {
+        word temp;
+        temp.random_string = random_string + x.random_string;
+        return temp;
+    } 
+    void operator == (word x)
+    {
+        if (random_string.length()==x.random_string.length())
+        {
+            cout<<"\n\nLength is equal"<<endl;
+        }
+        else
+        {
+            cout<<"\n\nNot equal"<<endl;
+        }
     }
 
+    void operator = (word x)
+    {
+        random_string = x.random_string;
+    }
 };
+
+int main()
+{
+    word O1,O2(" Ali"),O3;
+    O3 = O1+O2;
+    O1.display();
+    O2.display();
+    O3.display();     //Sum of O1 and O2
+    O1==O2;
+    O1=O2;
+    O1.display();
+    return 0;
+}
